ff9230bb0e8e7cca92eafc9e84bf9bf9eb4e0201
monika <kaczewska.mon@gmail.com>
2023-05-13 10:55:47 +0200

    initial commit

---
+++ src/test/java/com/example/bestcrmintheuniverse/BestCrmInTheUniverseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bestcrmintheuniverse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BestCrmInTheUniverseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
